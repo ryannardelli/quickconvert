c30429cabcbf3b3f8501ebe75c5ecbd9ea326144
Ryan <ryannardelli12@gmail.com>
2024-05-19 21:57:01 -0300

    enviando alterações

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const input_value = document.querySelector('#value');
+const msg = document.querySelector('.invalid-feedback');
+
 (function() {
     'use strict';
     const forms = document.querySelectorAll('.needs-validation');
@@ -6,17 +9,27 @@
             if (!form.checkValidity()) {
                 event.preventDefault();
                 event.stopPropagation();
-          }
+            } else {
+                event.preventDefault();
+            }
           form.classList.add('was-validated');
         }, false);
     });
 })();
 
-const input_value = document.querySelector('#value');
-
 input_value.addEventListener('input', (event) => {
     const value = event.target.value.trim();
+    if (value === '') {
+        msg.textContent = 'O campo não pode ficar em branco';
+        input_value.setCustomValidity('Invalid');
+    } else if (!/^\d+$/.test(value)) {
+        msg.textContent = 'O campo deve conter apenas números';
+        input_value.setCustomValidity('Invalid');
+        console.log('contém letras');
+    } else {
+        msg.textContent = '';
+        input_value.setCustomValidity('');
+    }
+    input_value.reportValidity();
     console.log(value);
-})
-
-// document.querySelector('.invalid-feedback').innerHTML = 'hello';
+});
